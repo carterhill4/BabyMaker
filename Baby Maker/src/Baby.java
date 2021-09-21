a4b660be96c76c30cb0c64748055164e4c9413c0
carter.hill <carter.hill@MU-C1-05.mullen.pvt.k12.co.us>
2021-09-21 11:27:26 -0600

    baby creator

---
+++ Baby Maker/src/Baby.java
@@ -0,0 +1,21 @@
+
+public class Baby
+	{
+			
+		String name;
+		double weight;
+		boolean isAdopted;
+	
+
+		public Baby (String n, double w, boolean a)
+			{
+				name = n;
+				weight = w;
+				isAdopted = a;
+			}
+		public static void score()
+		{
+			System.out.println("N'GOLO with the GOALLLLL!");
+		}
+
+}
---
+++ Baby Maker/src/BabyMaker.java
@@ -0,0 +1,23 @@
+
+public class BabyMaker
+	{
+
+		public static void main(String[] args)
+			{
+				
+				Baby myB1 = new Baby ("N'Golo", 8.2, true);
+				
+				System.out.println("My first baby is " + myB1.name);
+				
+				Baby myB2 = new Baby ("Carl", 7.9, false);
+				
+				System.out.println("My SECOND baby is " + myB2.name);
+				
+				if(myB1.isAdopted || myB2.isAdopted)
+					{
+						System.out.println("He is loved.");
+					}
+				myB1.score();
+			}
+
+	}
